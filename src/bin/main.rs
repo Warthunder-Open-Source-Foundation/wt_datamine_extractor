a8fe88409d130c0ed8a557728ea02f8db4d66847
flareflo <florianlipp17@gmail.com>
2021-11-08 12:19:17 +0100

    New missile update
    Took 4 minutes

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,8 @@
+use wt_missile_calc_lib::extract::extract_known;
 use wt_missile_calc_lib::missiles::{generate_raw, PATH};
 
 fn main() {
+	// extract_known();
 	generate_raw(PATH);
 	println!("Generated new all.json");
 }
--- src/lib/extract.rs
+++ src/lib/extract.rs
@@ -14,9 +14,9 @@ impl KnownMissiles {
 }
 
 pub fn extract_known() {
-	let known_raw = fs::read_to_string("../../index/known.json").unwrap();
+	let known_raw = fs::read_to_string("index/known.json").unwrap();
 	let known: KnownMissiles = serde_json::from_str(&known_raw).unwrap();
-	for (_, known) in known.path.iter().enumerate() {
+	for known in known.path {
 		let path = format!("resources/cache/aces.vromfs.bin_u/gamedata/weapons/rocketguns/{}", known);
 		if let Ok(contents) = fs::read(path) {
 			fs::write(format!("index/missiles/{}", known), contents).unwrap();
