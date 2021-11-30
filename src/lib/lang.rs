53df29f8ccd3abe418f5c9bc78f1be630ae60ff3
FlareFlo <florianlipp17@gmail.com>
2021-11-30 20:31:35 +0100

    Fixed duplicate regex problems
    Took 18 minutes

--- src/lib/lang.rs
+++ src/lib/lang.rs
@@ -25,12 +25,24 @@ pub fn unit_to_local(target: &str, path: &str) -> String {
 		.build()
 		.unwrap();
 
+	let shop_regex = RegexBuilder::new(&format!("{}_shop", target))
+		.case_insensitive(true)
+		.build()
+		.unwrap();
+
 	let mut found: usize;
+
+	if let Some(line) = edge_case_localize_before(&target) {
+		return line.to_owned();
+	}
+
 	if let Some(line) = short_regex.find(&unit_string) {
+		found = line.start();
+	} else if let Some(line) = shop_regex.find(&unit_string) {
 		found = line.start();
 	} else if let Some(line) = regex.find(&unit_string) {
 		found = line.start();
-	} else if let Some(line) = edge_case_localize(&target) {
+	} else if let Some(line) = edge_case_localize_after(&target) {
 		return line.to_owned();
 	} else {
 		eprintln!("target = {:?}", target);
@@ -42,13 +54,33 @@ pub fn unit_to_local(target: &str, path: &str) -> String {
 	trimmed
 }
 
-fn edge_case_localize(raw: &str) -> Option<&str> {
+fn edge_case_localize_after(raw: &str) -> Option<&str> {
 	match raw {
 		"us_fim-92b" => {
 			Some("Fim-92B")
 		}
 		"space_rocket_launcher" => {
 			Some("Space rocket launcher")
+		}
+		_ => {
+			None
+		}
+	}
+}
+
+fn edge_case_localize_before(raw: &str) -> Option<&str> {
+	match raw {
+		"us_m1a1_abrams_yt_cup_2019" => {
+			Some("M1A1 YT cup")
+		}
+		"germ_leopard_2a5_yt_cup_2019" => {
+			Some("Leopard 2A5 YT cup")
+		}
+		"uk_challenger_ii_yt_cup_2019" => {
+			Some("Challenger 2 YT cup")
+		}
+		"ussr_t_80u_yt_cup_2019" => {
+			Some("T-80U YT cup")
 		}
 		_ => {
 			None
