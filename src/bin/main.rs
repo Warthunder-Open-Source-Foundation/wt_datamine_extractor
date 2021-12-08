b3163dc36f7483cfa2b1722bcec651640d9b6cc7
flareflo <florianlipp17@gmail.com>
2021-12-08 10:48:12 +0100

    Bump version and add new set of version control
    Took 18 minutes

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,6 +14,9 @@ fn main() {
 	let start = Instant::now();
 
 	if fs::read_dir("resources/cache").is_ok() {
+		fs::write("meta_index/version.txt", &fs::read_to_string("resources/cache/aces.vromfs.bin_u/version").unwrap()).unwrap();
+
+
 		extract_csv();
 
 		let known_missiles = KnownMissiles::generate_index().write_index().copy_index_to_folder();
