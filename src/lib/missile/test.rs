9a33f3910010a8e9f5a86e96cb2c90851f935797
FlareFlo <florianlipp17@gmail.com>
2021-12-26 17:19:35 +0100

    Added full tests to missiles
    Took 22 minutes

--- src/lib/missile/test.rs
+++ src/lib/missile/test.rs
@@ -10,16 +10,158 @@ mod tests {
     static ref MISSILES: Vec<Missile> = {
 			let file = std::fs::read_to_string("missile_index/known.json").unwrap();
 			let known_missiles: KnownMissiles = serde_json::from_str(&file).unwrap();
-        let missiles = Missile::generate_from_index(&known_missiles);
+			let missiles = Missile::generate_from_index(&known_missiles);
 			missiles
 		};
 	}
 
 	#[test]
-	fn has_seeker_fov() {
+	fn has_name() {
 		for missile in MISSILES.iter() {
-			if missile.fov == 0.0 {
-				panic!("{} has no fov", missile.name)
+			if missile.name.is_empty() {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_localized_name() {
+		for missile in MISSILES.iter() {
+			if missile.localized.is_empty() {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_mass() {
+		for missile in MISSILES.iter() {
+			if missile.mass == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_end_mass() {
+		for missile in MISSILES.iter() {
+			if missile.mass_end == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_caliber() {
+		for missile in MISSILES.iter() {
+			if missile.caliber == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_force0_and_time_fire0() {
+		for missile in MISSILES.iter() {
+			if missile.force0 == 0.0 || missile.timefire0 == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn time_fire1_require_force1() {
+		for missile in MISSILES.iter() {
+			if missile.force1 != 0.0 {
+				if missile.timefire1 == 0.0 {
+					panic!("{}", missile.name)
+				}
+			}
+			if missile.timefire1 != 0.0 {
+				if missile.force1 == 0.0 {
+					panic!("{}", missile.name)
+				}
+			}
+		}
+	}
+
+	#[test]
+	fn has_cxk() {
+		for missile in MISSILES.iter() {
+			if missile.cxk == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_cx() {
+		for missile in MISSILES.iter() {
+			if missile.dragcx == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_time_life() {
+		for missile in MISSILES.iter() {
+			if missile.timelife == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_exp_mass() {
+		for missile in MISSILES.iter() {
+			if missile.exp_mass == 0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_lock_angle_max() {
+		for missile in MISSILES.iter() {
+			if missile.lockanglemax == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_angle_max() {
+		for missile in MISSILES.iter() {
+			if missile.anglemax == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_warm_up_time() {
+		for missile in MISSILES.iter() {
+			if missile.warmuptime == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_work_time() {
+		for missile in MISSILES.iter() {
+			if missile.worktime == 0.0 {
+				panic!("{}", missile.name)
+			}
+		}
+	}
+
+	#[test]
+	fn has_delta_v() {
+		for missile in MISSILES.iter() {
+			if missile.deltav == 0.0 {
+				panic!("{}", missile.name)
 			}
 		}
 	}
