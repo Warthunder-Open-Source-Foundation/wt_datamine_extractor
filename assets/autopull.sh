f6e4162a77be4b7e3841324998219ddab64ae057
FlareFlo <mail@flareflo.dev>
2022-04-15 18:48:20 +0200

    Optimized pull script

--- assets/autopull.sh
+++ assets/autopull.sh
@@ -2,11 +2,14 @@
 cd .. &&
 OLD=`cat meta_index/version.txt` &&
 cd resources/cache &&
-git pull &&
-cd ../.. && 
-cargo run --release && cargo test --release &&
-NEW=`cat meta_index/version.txt` &&
-semver_cli --cmp $OLD:$NEW --exit &&
-git add . &&
-git commit -m "automatically update to ${NEW}" &&
-git push
+GIT_OUT=$(git pull 2>&1) &&
+if [ "$GIT_OUT"  != "Already up to date." ]; then
+  cd ../.. &&
+  cargo run --release && cargo test --release &&
+  NEW=`cat meta_index/version.txt` &&
+  semver_cli --cmp $OLD:$NEW --exit &&
+  git add . &&
+  git commit -m "automatically update to ${NEW}" &&
+  git push
+fi
+echo "Git is already up to date"
