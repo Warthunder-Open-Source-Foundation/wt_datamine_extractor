23ca391b46320ff8b9bc1220d3aa596d48fd1090
FlareFlo <mail@flareflo.dev>
2022-08-18 20:40:52 +0200

    Add localization to BRs

--- src/lib/battle_rating/battle_rating.rs
+++ src/lib/battle_rating/battle_rating.rs
@@ -2,11 +2,13 @@ use std::fs;
 
 use serde::{Deserialize, Serialize};
 
-use crate::battle_rating::battle_rating_def::{BattleRating};
+use crate::battle_rating::battle_rating_def::BattleRating;
+use crate::lang::{Lang, name_to_local};
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct VehicleBattleRating {
 	pub name: String,
+	pub localized: String,
 	pub arcade: BattleRating,
 	pub realistic: BattleRating,
 	pub simulator: BattleRating,
@@ -18,17 +20,18 @@ impl VehicleBattleRating {
 		let raw = fs::read_to_string(path).unwrap();
 		let mut items: Vec<VehicleBattleRating> = vec![];
 		let mut current = VehicleBattleRating::default();
-		for line in  raw.split('\n').collect::<Vec<&str>>() {
+		for line in raw.split('\n').collect::<Vec<&str>>() {
 			// Indentation based detection of a new vehicle
 			if line.starts_with(r#"  ""#) && line.contains(r#"": {"#) {
 				current = VehicleBattleRating::default();
 
 				current.name = line.split_at(3).1.split('"').next().unwrap().to_owned();
+				current.localized = name_to_local(&current.name, &Lang::Unit);
 			}
 			// Store latest and reset
 			if line.starts_with("  },") {
 				items.push(current.clone());
-				continue
+				continue;
 			}
 
 			let get_line = |line: &str| {
