086e8613f818257f809dbdf140f34ff766d76c54
FlareFlo <mail@flareflo.dev>
2022-09-17 21:47:28 +0200

    Add nationality and rank string compatibility

---
+++ src/lib/battle_rating/rank.rs
@@ -0,0 +1,37 @@
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+use const_gen::CompileConst;
+use get_size::GetSize;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, Default, Hash, PartialEq, Eq, CompileConst)]
+pub struct Rank(u8);
+
+impl Rank {
+	pub fn new(rank: u8) -> Self {
+		Self(rank)
+	}
+}
+
+impl Display for Rank {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}", roman::to(self.0.into()).expect("This should never overflow, as the into call safely wraps"))
+	}
+}
+
+impl FromStr for Rank {
+	type Err = ();
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		if let Some(rank) = roman::from(s) {
+			let parsed = u8::try_from(rank).or_else(|_|Err(()))?;
+			Ok(Self::new(parsed))
+		} else {
+			Err(())
+		}
+	}
+}
+
+impl GetSize for Rank {
+
+}
